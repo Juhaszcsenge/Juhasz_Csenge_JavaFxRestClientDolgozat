9ae3d328e2131dc0825ad23e18b1e6f634718c2e
Csenge <csenge799@gmail.com>
2022-11-26 00:09:41 +0100

    Feladat befejezése asszem, de nem tudom futattni mert szar a gépem :)

--- src/main/java/com/example/juhasz_csenge_javafxrestclientdolgozat/Update.java
+++ src/main/java/com/example/juhasz_csenge_javafxrestclientdolgozat/Update.java
@@ -1,13 +1,16 @@
 package com.example.juhasz_csenge_javafxrestclientdolgozat;
 
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 import java.io.IOException;
+
 public class Update extends Controller {
     @FXML
     private TextField UrallomasField;
@@ -28,6 +31,38 @@ public class Update extends Controller {
                 new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 200, 30);
         LetszamField.setValueFactory(valueFactory);
     }
+    @FXML
     public void updateClick(ActionEvent actionEvent) {
+        String Urallomas = UrallomasField.getText().trim();
+        String Jelentkezok = JelentkezokField.getText().trim();
+        String Email = EmailField.getText().trim();
+        int Letszam = LetszamField.getValue();
+        if (Urallomas.isEmpty()){
+            warning("Állomás megadása kötelező");
+            return;
+        }
+        if(Email.isEmpty()){
+            warning("Email megadása kötelező");
+            return;
+        }
+    }
+    this.Csillagaszat.setUrallomas(Urallomas);
+    this.Csillagaszat.setjelentkezok(jelentkezok);
+    this.Csillagaszat.setLetszam(Letszam);
+    this.Csillagaszat.setEmail(Email);
+    Gson converter = new Gson();
+    String json = converter.toJson(this.Csillagaszat);
+    try{
+        String url = App.BASE_URL + "/" + this.Csillagaszat.getId();
+        Response response = RequestHandler.put(url, json);
+        if (response.getResponseCode() == 200){
+            Stage stage = (Stage) this.updateButton.getScene().getWindow();
+            stage.close();
+        }
+        else {
+            error("Hiba történik amódosítás során", response.getContent());
+        }
+    } catch (IOException e){
+        error("Nem sikerült kapcsolódni a szerverhez");
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module com.example.juhasz_csenge_javafxrestclientdolgozat {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.google.gson;
 
-
-    opens com.example.juhasz_csenge_javafxrestclientdolgozat to javafx.fxml;
+    opens com.example.juhasz_csenge_javafxrestclientdolgozat to javafx.fxml, com.google.gson;
     exports com.example.juhasz_csenge_javafxrestclientdolgozat;
 }
